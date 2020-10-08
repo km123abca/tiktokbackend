304143acc1b1ce1782b7e9d80b35b3f4db11a072
unknown <km123abca>
2020-10-08 07:37:21 +0530

    fifth attempt

--- dbModel.js
+++ dbModel.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+import mongoose from "mongoose";
 const tiktokSchema = mongoose.Schema({
   url: String,
   channel: String,
